a193321d49c8c202c12f0f135f2fd267c18d7b8d
Cocoa <r.xu.1@research.gla.ac.uk>
2022-07-30 13:50:18 +0100

    add elixir_make to deps

--- mix.exs
+++ mix.exs
@@ -1,28 +1,23 @@
-defmodule CcPrecompiler.MixProject do
+defmodule CCPrecompiler.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :cc_precompiler,
       version: "0.1.0",
-      elixir: "~> 1.14-dev",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    [{:elixir_make, "~> 0.6", runtime: false}]
   end
 end
