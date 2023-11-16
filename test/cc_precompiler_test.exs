6c02cc1a59ba7e98570c5cbf08d4c7c47a083927
Cocoa <i@uwucocoa.moe>
2023-11-16 21:52:42 +0800

    mix format

--- test/cc_precompiler_test.exs
+++ test/cc_precompiler_test.exs
@@ -5,17 +5,17 @@ defmodule Mix.Tasks.ElixirMake.CCPrecompiler.Test do
   describe "all_supported_targets when fetching" do
     test "without include_default_ones" do
       assert [
-        "aarch64-apple-darwin",
-        "x86_64-apple-darwin",
-        "aarch64-linux-gnu",
-        "armv7l-linux-gnueabihf",
-        "i686-linux-gnu",
-        "powerpc64le-linux-gnu",
-        "riscv64-linux-gnu",
-        "s390x-linux-gnu",
-        "x86_64-linux-gnu",
-        "x86_64-windows-msvc"
-      ] == CCPrecompiler.all_supported_targets(:fetch)
+               "aarch64-apple-darwin",
+               "x86_64-apple-darwin",
+               "aarch64-linux-gnu",
+               "armv7l-linux-gnueabihf",
+               "i686-linux-gnu",
+               "powerpc64le-linux-gnu",
+               "riscv64-linux-gnu",
+               "s390x-linux-gnu",
+               "x86_64-linux-gnu",
+               "x86_64-windows-msvc"
+             ] == CCPrecompiler.all_supported_targets(:fetch)
     end
   end
 end
